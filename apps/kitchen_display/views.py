28ba220b063b231b01231aaa711afd1ecafd420c
lifanwar <mualifulanwar1@gmail.com>
2026-02-23 00:44:38 +0200

    Remove Commentar

--- apps/kitchen_display/views.py
+++ apps/kitchen_display/views.py
@@ -1,41 +1,6 @@
 from django.views.generic import ListView
 from apps.point_off_sale.models import Order
 
-
-# class StationOrderListMixin(ListView):
-#     model = Order
-#     template_name = "kitchen_display/displaykitchen.html"
-#     context_object_name = "orders"
-
-#     prep_station = None  # 'kitchen' atau 'bar'
-
-#     def get_queryset(self):
-#         if self.prep_station is None:
-#             raise ValueError("prep_station harus di-set di subclass")
-
-#         qs = (
-#             Order.objects
-#             .filter(status="open")
-#             .prefetch_related(
-#                 "items",
-#                 "items__menu_item",
-#                 "items__menu_item__group",
-#             )
-#             .select_related("table")
-#         )
-
-#         # Hanya order yang punya item untuk station ini
-#         qs = [
-#             o for o in qs
-#             if o.items.filter(menu_item__prep_station=self.prep_station).exists()
-#         ]
-#         return qs
-
-#     def get_context_data(self, **kwargs):
-#         ctx = super().get_context_data(**kwargs)
-#         ctx["station"] = self.prep_station
-#         return ctx
-
 class StationOrderListMixin(ListView):
     model = Order
     template_name = "kitchen_display/displaykitchen.html"
