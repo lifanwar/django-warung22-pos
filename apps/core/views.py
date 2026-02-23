30dd754f44ab5c86b767f174e6eb3f2c3b4d89a3
lifanwar <mualifulanwar1@gmail.com>
2026-02-23 04:16:54 +0200

    Add handling login

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse
 
-# Create your views here.
+class login(LoginView):
+    template_name = 'core/login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        # Ini akan selalu mengarahkan ke kitchen-display setelah login
+        return reverse('kitchen_display:kitchen-display')
+    
