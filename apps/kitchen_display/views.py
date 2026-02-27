34cf6ae74bb94c36d4b54020ef4bf580aa776bd2
lifanwar <mualifulanwar1@gmail.com>
2026-02-27 14:50:51 +0200

    Update import from order instead pos apps

--- apps/kitchen_display/views.py
+++ apps/kitchen_display/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from apps.point_off_sale.models import Order, OrderItem
+from apps.order.models import Order, OrderItem
 from django.http import HttpResponseBadRequest
 from django.shortcuts import get_object_or_404, redirect
 
