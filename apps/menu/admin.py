6dcf7596f5ec62ac73ae82135cc0cc90d1c6deb4
lifanwar <mualifulanwar1@gmail.com>
2026-02-27 15:00:57 +0200

    Move all menu to menu apps

--- apps/menu/admin.py
+++ apps/menu/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import MenuCategory, MenuGroup, MenuItem
 
 # Register your models here.
+
+@admin.register(MenuCategory)
+class MenuCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(MenuGroup)
+class MenuGroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "category__name")
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "group", "get_category", "price", "prep_station", "is_available")
+    list_filter = ("prep_station", "is_available", "group__category")
+    search_fields = ("name", "group__name", "group__category__name")
+    list_editable = ("is_available",)
+
+    def get_category(self, obj):
+        return obj.category.name
+    get_category.short_description = "Category"
--- apps/order/admin.py
+++ apps/order/admin.py
@@ -3,11 +3,9 @@ from django.contrib import admin
 from .models import (
     UserProfile,
     Table,
-    MenuItem,
     Order,
     OrderItem,
 )
-from apps.menu.models import  MenuCategory, MenuGroup
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
@@ -20,30 +18,6 @@ class TableAdmin(admin.ModelAdmin):
     list_display = ("number", "capacity", "is_active")
     list_filter = ("is_active",)
     search_fields = ("number",)
-
-@admin.register(MenuCategory)
-class MenuCategoryAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-    search_fields = ("name",)
-
-
-@admin.register(MenuGroup)
-class MenuGroupAdmin(admin.ModelAdmin):
-    list_display = ("name", "category")
-    list_filter = ("category",)
-    search_fields = ("name", "category__name")
-
-
-@admin.register(MenuItem)
-class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ("name", "group", "get_category", "price", "prep_station", "is_available")
-    list_filter = ("prep_station", "is_available", "group__category")
-    search_fields = ("name", "group__name", "group__category__name")
-    list_editable = ("is_available",)
-
-    def get_category(self, obj):
-        return obj.category.name
-    get_category.short_description = "Category"
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
